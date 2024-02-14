84ced7cae7f56bf0e48fbfe9d695ee48d793967d
Geek Code <rovafiti@email.com>
2024-02-14 21:38:17 +0300

    Init syntaxe Jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+const title = "My title page"
+const style = {color:'red', backgroundColor: '#ededed'}
 function App() {
-  return "Hello world"
+  const handleClick = () => {
+    alert('Hello guys') 
+  }
+  return <>
+    {/* Pour les style on doit le mettre sous forme d'objet */}
+    <h1 onClick={handleClick} id="title" className="title" style={style}>{title}</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo harum et distinctio, ad minus voluptate odio quasi aut blanditiis minima quam voluptatem dolore laborum exercitationem, excepturi facilis dolores. Est, at?</p>
+  </>
 }
 
 export default App
