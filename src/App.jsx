6356d0b72808ed11ba8889da5321c5623d471917
Geek Code <rovafiti@email.com>
2024-02-15 22:50:31 +0300

    Second example of Hook useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  //   const [count, setCount] = useState(0)
+
+  const [person, setPerson] = useState({
+    firstName: "John",
+    lastName: "Doe",
+    age: 18,
+  });
 
   const increment = () => {
-	setCount(count + 1)
+    //setCount(count + 1)
+    setPerson({ ...person, age: person.age + 1 });
   };
-
   return (
     <>
-      <p>Conteur : {count}</p>
+      <p>Age de {person.firstName} : {person.age}</p>
       <button onClick={increment}>Increment</button>
     </>
   );
